3e6be6157a26f24392c48f557cd1b29cdea3cd50
akr7777 <dimitrius77@protonmail.com>
2025-01-17 01:36:33 +0300

    roter modified - step 2

--- src/admin/pages/not-found-page/not-found.tsx
+++ src/admin/pages/not-found-page/not-found.tsx
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router'
-import { PATHS } from '../../router/router'
+import { PATHS } from '../../../router/router'
 
 import './not-found.scss'
 
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,7 +1,5 @@
 import { Routes, Route } from "react-router";
-import { MainAppPage } from "../pages/main-app-page/main-app-page";
 import { AuthPage } from "../admin/pages/auth/auth-page";
-import { NotFoundPage } from "../pages/not-found-page/not-found";
 import { MainAdminPage } from "../admin/pages/main-admin-page/main-admin-page";
 import { PrivateRoute } from "./private-router";
 import { AdminNewsPage } from "../admin/pages/news/admin-news-page";
@@ -11,16 +9,11 @@ import { SeparateEventPage } from "../admin/pages/events/separate-event-page";
 import { AdminEventsPage } from "../admin/pages/events/admin-event-page";
 import { GidsPage } from "../admin/pages/gids/all-gids";
 import { SeparateGidPage } from "../admin/pages/gids/separate-gid/saparate-gid";
+import { NotFoundPage } from "../admin/pages/not-found-page/not-found";
 
 export const PATHS = {
   rootPath: '/',
   auth: '/auth/',
-  // admin: {
-  //   root: '/admin',
-  //   events: '/admin/events/',
-  //   news: '/admin/news/',
-  //   gids: '/admin/gids/',
-  // },
   events: '/events/',
   news: '/news/',
   gids: '/gids/',
@@ -33,9 +26,10 @@ export const AppRouter = () => {
 
     return (
       <Routes>
-        {/* <Route path={PATHS.rootPath} element={<MainAppPage />} /> */}
-        {/* <Route index element={<MainAppPage />} /> */}
-        <Route index element={<MainAdminPage />} />
+        {/* <Route index element={<MainAdminPage />} /> */}
+
+        <Route path={PATHS.auth} element={<AuthPage />} />
+
 
         <Route path={PATHS.rootPath} element={
           <PrivateRoute element={<MainAdminPage />}/>
@@ -50,34 +44,8 @@ export const AppRouter = () => {
           <Route path={PATHS.gids + ":gidId"} element={<SeparateGidPage />} />
         </Route>
 
-        
-
-
-        <Route 
-            path={PATHS.auth} 
-            element={<AuthPage />} 
-          />
-
-        {/* <Route path={PATHS.admin.root} element={
-          <PrivateRoute element={<MainAdminPage />}/>
-        }> */}
-
-         
-          {/* <Route path={PATHS.admin.news} element={<AdminNews />}>
-            <Route path={":newId"} element={<AdminSeparateNew />} />
-          </Route> */}
-          {/* <Route path={PATHS.admin.news} element={<AdminNewsPage />} />
-          <Route path={PATHS.admin.news + ":newId"} element={<SeparateNewPage />} />
-
-          <Route path={PATHS.admin.events} element={<AdminEventsPage />} />
-          <Route path={PATHS.admin.events + ":eventId"} element={<SeparateEventPage />} />
-
-          <Route path={PATHS.admin.gids} element={<GidsPage />} />
-          <Route path={PATHS.admin.gids + ":gidId"} element={<SeparateGidPage />} /> */}
-
-        {/* </Route> */}
-
         <Route path="*" element={<NotFoundPage />} />
+
       </Routes>
     )
 }
